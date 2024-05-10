e95df922f4af6e68996578a9a66aabbb18f8ec95
ehandresen <eirik.horgen.andresen@gmail.com>
2024-05-10 16:41:11 +0200

    added body parser middleware

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
+// body parser middleware
+app.use(express.json()); // takes care of submitting raw json
+app.use(express.urlencoded({ extended: false })); // typically form-data
+
 // routes
 // common convention is to prefix your json api with /api
 app.use('/api/posts', posts);
