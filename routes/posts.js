0635cd5125538881968f26cda77a000ff07481b5
ehandresen <eirik.horgen.andresen@gmail.com>
2024-05-10 17:21:17 +0200

    added PUT method

--- routes/posts.js
+++ routes/posts.js
@@ -45,6 +45,18 @@ router.post('/', (req, res) => {
   res.status(201).json(posts);
 });
 
-//
+// update post
+router.put('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id); // 'post' is now a reference to the original object in the array
+  console.log(post);
+
+  if (!post) {
+    return res.status(404).json({ msg: `No post with id ${id}` });
+  }
+
+  post.title = req.body.title;
+  res.status(200).json(posts);
+});
 
 export default router;
